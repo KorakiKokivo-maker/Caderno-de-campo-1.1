4639ada16a9495be4ae81f05ab0c0f62e74cf082
KorakiKokivo-maker <jefersonbitellojunior@gmail.com>
2025-10-29 11:55:36 -0300

    Create database.ts

---
+++ database.ts
@@ -0,0 +1,5 @@
+export { addUsuario as addUser, getUsuarios as getUsers, updateUsuario as updateUser } from './mockDatabase';
+export { addPropriedade, getPropriedadesByUsuario, updatePropriedade, deletePropriedade } from './mockDatabase';
+export { addColheita, getColheitasBySafra, updateColheita, deleteColheita } from './mockDatabase';
+export { addBenfeitoria, getBenfeitoriasByUsuario, updateBenfeitoria, deleteBenfeitoria } from './mockDatabase';
+export { addSafra, getSafrasByUsuario, updateSafra, deleteSafra } from './mockDatabase';
